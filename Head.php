fe5e59c682388cb7687fe4ef6ac57fcc87a12eef
Hanna Landerus <hannalanderus@Hannas-MacBook-Air-2.local>
2017-10-27 12:16:00 +0200

    style

---
+++ Head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+     <script src="https://use.fontawesome.com/67b80e2b65.js"></script>
+     <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
+     
+</head>
--- index.php
+++ index.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-     <script src="https://use.fontawesome.com/67b80e2b65.js"></script>
-     
-</head>
+
 <body>
   <h1>ToDoList</h1>
   <!--Länkar till dokument med kod för de olika "funktionerna" i uppgiften-->
    <?php
+    require 'Head.php';
     require 'databas.php';
     require 'add.php';
     require 'Delete.php';
@@ -33,8 +26,8 @@
     require 'fetch.php'; 
     foreach ($todo as $row):
     if ($row['Completed']==0){ ?>
-<div class="wrapper">
-    <div class="task"><?php echo $row ['Title']; ?></div>
+ <div class="wrapper">
+ <div class="task"><?php echo $row ['Title']; ?></div>
 
   
 
@@ -50,10 +43,10 @@
 
 <div class="delete">
 <!--kod knapp för delete delen-->
-<form method="POST" action="Delete.php">
-<input type="hidden" name="Delete" value="<?php echo $row['Id'] ?>">
-<input type= "submit" value="Delete">
-</form>
+  <form method="POST" action="Delete.php">
+    <input type="hidden" name="Delete" value="<?php echo $row['Id'] ?>">
+    <input type= "submit" value="Delete">
+  </form>
 </div>  
 </div> <!--div till wrapper-->
 <?php  } endforeach; ?><!--endforeach hör till : uppe vid delen som skriver ut uppgifterna, här avslutas det-->
@@ -61,24 +54,24 @@
 
  <!--Detta avser inputfält för nya uppgifter-->  
 <div class="addtask">
-<h2>Add ToDo</h2>
-<form method="POST">
-<p>Task:<div class="a"><input type="text" name="Title"/></div>
-<p>CreatedBy:<div class="b"><input type="text" name="CreatedBy"/></div>
-<p><input type="submit" value="Add New"/>
-<a href="index.php">Cancel</a></p>
-</form>
+  <h2>Add ToDo</h2>
+    <form method="POST">
+        <p>Task:<div class="do"><input type="text" name="Title"/></div>
+        <p>CreatedBy:<div class="by"><input type="text" name="CreatedBy"/></div>
+        <p><input type="submit" value="Add New"/>
+        <a href="index.php">Cancel</a></p>
+    </form>
 </div>
 
-  <div class= "completed">  
+<div class= "completed">  
    <?php
-   foreach($todo as $row){
-       if ($row['Completed']==1){
-         echo $row ['Title'] . 'CHECK' . '</br>';
+        foreach($todo as $row){
+        if ($row['Completed']==1){
+        echo $row ['Title'] ." / / ". 'Done and Done' . '</br>';
        }
        }  
    ?>
-   </div>
+</div>
     
     
 </body>
